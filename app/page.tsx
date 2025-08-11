2d9a70264fa876f03d121f6633b71f58d5d6c469
Mike Melville <mrmelville3@gmail.com>
2025-08-11 05:33:49 -0400

    fixed width of <p> on home page for mobile.

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     return (
     <div>
       <PageHeading title="Welcome" description="This is the Melvi-tron." />
-      <p className="text-center mx-auto w-1/3">I built this web application using React and Next.js to demonstrate various concepts of machine learning and artificial intelligence. Click around, have fun, and learn something.</p>
+      <p className="text-center mx-auto px-9 lg:w-1/2">I built this web application using React and Next.js to demonstrate various concepts of machine learning and artificial intelligence. Click around, have fun, and learn something.</p>
       <div className="sm:w-1/5 w-1/3 mx-auto my-4">
         <MyGraph2 xMin={-50} xMax={50} yMin={-50} yMax={50} graphDataSets={[]} uid='1' />
       </div>
